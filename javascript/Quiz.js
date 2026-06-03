35945f62eefb0f9dc842d3efe9493652770589c2
RafaelIenne <rafaiennemanoel@gmail.com>
2026-06-02 23:49:08 -0300

    Declaração das variaveis do quiz

---
+++ javascript/Quiz.js
@@ -0,0 +1,4 @@
+// declaração dos campos
+let Pergunta = document.querySelector("#pergunta");
+let respostasContainer = document.querySelector("#respostas");
+let mensagem = document.querySelector("#mensagem");
